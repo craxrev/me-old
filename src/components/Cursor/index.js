a5838deef5a48ced8f8c0518a3f20a0b80bbce91
craxrev <mortadha.ghanmi56@gmail.com>
2021-04-29 04:29:34 +0200

    add basic noise and link scale logic

--- src/components/Cursor/index.js
+++ src/components/Cursor/index.js
@@ -7,8 +7,12 @@ const Cursor = () => {
     const cursorItselfRef = useRef()
 
     useEffect(() => {
+        // TODO: export below logic into a separate file, maybe refactor into a javascript class?
+        // TODO: extract helper functions
+        // TODO: extract configurations & variables
+        // TODO: make some configuration optional
         if (cursorCompanionRef.current && cursorItselfRef.current) {
-            let timeout
+            let timeout, noiser
             let timeup = true
 
             const cursorCompanionEl = cursorCompanionRef.current
@@ -23,6 +27,12 @@ const Cursor = () => {
                 x: 0,
                 y: 0,
             }
+            const noise = {
+                x: 0,
+                y: 0,
+            }
+            let cursorScale = 1
+            let scale = 1
             const skew = {
                 x: 0,
                 y: 0,
@@ -48,10 +58,15 @@ const Cursor = () => {
                 }, 200)
             }
             const onMouseMoveStart = () => {
-                console.log("onMouseMoveStart")
+                clearInterval(noiser)
+                noise.x = 0
+                noise.y = 0
             }
             const onMouseMoveEnd = () => {
-                console.log("onMouseMoveEnd")
+                noiser = setInterval(() => {
+                    noise.x = (0.5 - Math.random()) * 4
+                    noise.y = (0.5 - Math.random()) * 4
+                }, 100)
             }
             const onMouseDown = () => {
                 console.log("onMouseDown")
@@ -59,22 +74,35 @@ const Cursor = () => {
             const onMouseUp = () => {
                 console.log("onMouseUp")
             }
-            const onMouseLeave = () => {
-                console.log("onMouseLeave")
+            const onMouseOut = e => {
+                const target = e.target
+                if (target && target.tagName.toLowerCase() === "a") {
+                    scale = 1
+                }
             }
-            const onMouseEnter = () => {
-                console.log("onMouseEnter")
+            const onMouseOver = e => {
+                const target = e.target
+                if (target && target.tagName.toLowerCase() === "a") {
+                    scale = 3
+                }
             }
             const update = () => {
-                cursorPos.x += (pos.x - cursorPos.x) * ease
-                cursorPos.y += (pos.y - cursorPos.y) * ease
+                if (noise.x === 0 && noise.y === 0) {
+                    cursorPos.x += (pos.x - cursorPos.x) * ease
+                    cursorPos.y += (pos.y - cursorPos.y) * ease
+                } else {
+                    cursorPos.x += (pos.x + noise.x - cursorPos.x) * (ease / 10)
+                    cursorPos.y += (pos.y + noise.y - cursorPos.y) * (ease / 10)
+                }
                 if (cursorPos.x < 0.01) cursorPos.x = 0
                 if (cursorPos.y < 0.01) cursorPos.y = 0
+
+                cursorScale += (scale - cursorScale) * ease
 
                 const diffX = Math.abs(cursorPos.x - pos.x)
                 const diffY = Math.abs(cursorPos.y - pos.y)
                 if (diffX >= 0.01 || diffY >= 0.01) {
-                    cursorCompanionEl.style.transform = `translate3d(${cursorPos.x}px, ${cursorPos.y}px, 0) skew(${skew.x}deg, ${skew.y}deg)`
+                    cursorCompanionEl.style.transform = `translate3d(${cursorPos.x}px, ${cursorPos.y}px, 0) skew(${skew.x}deg, ${skew.y}deg) scale(${cursorScale})`
                     cursorItselfEl.style.transform = `translate3d(${pos.x}px, ${pos.y}px, 0)`
                 }
 
@@ -85,16 +113,16 @@ const Cursor = () => {
             window.addEventListener("mousemove", onMouseMove)
             window.addEventListener("mousedown", onMouseDown)
             window.addEventListener("mouseup", onMouseUp)
-            window.addEventListener("mouseleave", onMouseLeave)
-            window.addEventListener("mouseenter", onMouseEnter)
+            window.addEventListener("mouseout", onMouseOut)
+            window.addEventListener("mouseover", onMouseOver)
 
             return () => {
                 cancelAnimationFrame(request)
                 window.removeEventListener("mousemove", onMouseMove)
                 window.removeEventListener("mousedown", onMouseDown)
                 window.removeEventListener("mouseup", onMouseUp)
-                window.removeEventListener("mouseleave", onMouseLeave)
-                window.removeEventListener("mouseenter", onMouseEnter)
+                window.removeEventListener("mouseout", onMouseOut)
+                window.removeEventListener("mouseover", onMouseOver)
             }
         }
     }, [])
