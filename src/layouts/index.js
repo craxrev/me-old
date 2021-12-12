87db62007771ee15c74b58ff06871cea89832cda
craxrev <mortadha.ghanmi56@gmail.com>
2021-12-12 14:26:14 +0300

    restore back custom cursors

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,18 @@
-import React from "react"
+import React, { Fragment } from "react"
 import { Helmet } from "react-helmet"
 
+import Cursor from "../components/Cursor"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+
+import useDeviceDetect from "../hooks/useDeviceDetect"
 
 import { GlobalStyle } from "../styles/Global"
 
 import { Wrapper } from "./styles"
 
 export default function Layout({ children }) {
+    const CursorElem = useDeviceDetect.isMobile ? Fragment : Cursor
     return (
         <>
             <Helmet>
@@ -19,6 +23,7 @@ export default function Layout({ children }) {
                 />
                 <meta name="color-scheme" content="dark" />
             </Helmet>
+            <CursorElem />
             <Wrapper>
                 <GlobalStyle />
                 <Header />
